dd530229a99135a755b003f4ab3ef61b3a676d6c
Al-magribi <almagribi.appdev@gmail.com>
2025-10-10 18:52:00 +0700

    Add dist/assets

---
+++ client/dist/assets/StudentDash-BA1Cw5WU.js
@@ -0,0 +1,2 @@
+import{j as s,h as t}from"./index-uqXiPZaM.js";import"./vendor-DTs43g_X.js";const e=()=>s.jsx(t,{title:"Dashboard",levels:["student"],children:"StudentDash"});export{e as default};
+//# sourceMappingURL=StudentDash-BA1Cw5WU.js.map
---
+++ client/dist/assets/StudentLms-Cwgl7u8N.js
@@ -0,0 +1,2 @@
+import{j as t,h as a}from"./index-uqXiPZaM.js";import"./vendor-DTs43g_X.js";const e=()=>t.jsx(a,{title:"Daftar Mata Pelajaran",levels:["student"],children:"StudentLms"});export{e as default};
+//# sourceMappingURL=StudentLms-Cwgl7u8N.js.map
